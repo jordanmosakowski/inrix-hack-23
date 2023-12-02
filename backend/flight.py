e1469e7ed42c5d2d918244948394033c51881d3b
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2023-12-02 14:24:33 -0800

    use cached data

--- backend/flight.py
+++ backend/flight.py
@@ -9,13 +9,13 @@ FLIGHT_API_KEY = os.getenv('FLIGHT_API_KEY')
 
 def search_flights(origin, destination, departure_date, return_date):
 
-    # return send_file('flight.json', mimetype='application/json')
+    return send_file('flight.json', mimetype='application/json')
 
-    url = f"https://api.flightapi.io/roundtrip/{FLIGHT_API_KEY}/{origin}/{destination}/{departure_date}/{return_date}/1/0/0/Economy/USD"
+    # url = f"https://api.flightapi.io/roundtrip/{FLIGHT_API_KEY}/{origin}/{destination}/{departure_date}/{return_date}/1/0/0/Economy/USD"
 
-    response = requests.get(url)
+    # response = requests.get(url)
     
-    if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return jsonify({'error': 'Request failed'}), response.status_code
+    # if response.status_code == 200:
+    #     return jsonify(response.json())
+    # else:
+    #     return jsonify({'error': 'Request failed'}), response.status_code
