5a671856823dfc9c4b2f3669e3c28a25f07c129e
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2023-12-02 14:47:47 -0800

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import './App.scss';
 import JetStreamMap from './JetStreamMap';
 import Welcome from './Welcome';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Welcome /> */}
-      <JetStreamMap />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Welcome />} />
+          <Route path="map" element={<JetStreamMap />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+      {/* <JetStreamMap /> */}
     </div>
   )
 }
