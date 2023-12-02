c22959d24b17c79b5980fb7e5985bca73f9a8c07
ChrisShobe <chrisshobe2000@gmail.com>
2023-12-02 15:51:01 -0800

    side info

--- frontend/src/FlightInfoPanel.js
+++ frontend/src/FlightInfoPanel.js
@@ -4,6 +4,9 @@ function FlightInfoPanel() {
     return (
         <div className="FlightInfoPanel">
             <h2>Flight Info</h2>
+            <h2>Flight Number</h2>
+            <h2>Departure Date and Time</h2>
+            <h2>Arrival Date and Time</h2>
         </div>
     )
 }
