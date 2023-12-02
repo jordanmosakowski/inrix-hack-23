08298d980eee1202fa15b44b46eae66ad173f210
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2023-12-02 14:21:13 -0800

    [backend] merge flight.py into main

--- backend/flight.py
+++ backend/flight.py
@@ -1,32 +1,21 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import jsonify, send_file
 import requests
 
 from dotenv import load_dotenv
 import os
 
 load_dotenv()  # This loads the environment variables from .env
-API_KEY = os.getenv('API_KEY')
+FLIGHT_API_KEY = os.getenv('FLIGHT_API_KEY')
 
-app = Flask(__name__)
-CORS(app)
+def search_flights(origin, destination, departure_date, return_date):
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World'
+    # return send_file('flight.json', mimetype='application/json')
 
-@app.route('/search-flights', methods=['GET'])
-def search_flights():
+    url = f"https://api.flightapi.io/roundtrip/{FLIGHT_API_KEY}/{origin}/{destination}/{departure_date}/{return_date}/1/0/0/Economy/USD"
 
-    url = f"https://api.flightapi.io/roundtrip/{API_KEY}/FRA/SFO/2024-01-08/2024-01-12/1/0/0/Economy/USD"
-
-    
     response = requests.get(url)
     
     if response.status_code == 200:
         return jsonify(response.json())
     else:
         return jsonify({'error': 'Request failed'}), response.status_code
-
-if __name__ == '__main__':
-    app.run(debug=True)
