dc3cb379ca2b5b410dbdf01ad5550004836b2124
sipneat <jake.esperson@outlook.com>
2023-12-02 11:06:12 -0800

    base deck.gl map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import DeckGL, { GeoJsonLayer } from 'deck.gl';
+import { Map } from 'react-map-gl';
+
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiamVzcGVyc29uIiwiYSI6ImNscG9ldXQ0ZDBwbTIya285bW9neGcxZG8ifQ.oN9Tsez9LV9y-vk5oxyryA"
+const MAP_STYLE = "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json";
+
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 10,
+  pitch: 30,
+  bearing: 0
+};
 
 function App() {
+
+  const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
+
+  const layers = [
+    new GeoJsonLayer({
+      id: 'airports',
+      data: 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson',
+      //Styles
+      filled: true,
+      pointRadiusMinPixels: 5,
+      pointRadiusScale: 2000,
+      getPointRadius: f => 5,
+      getFillColor: [86, 144, 58, 250],
+      pickable: true,
+      autoHighlight: true,
+    })
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DeckGL
+        viewState={viewState}
+        onViewStateChange={({ viewState }) => setViewState(viewState)}
+        controller={true}
+        layers={layers}
+      >
+        <Map
+          mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+          mapStyle={MAP_STYLE}
+        />
+      </DeckGL>
     </div>
   );
 }
