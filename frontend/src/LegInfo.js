6cf8c3eaf9b101b012a8e202fd8a7475050178d2
ChrisShobe <chrisshobe2000@gmail.com>
2023-12-02 23:20:45 -0800

    fixing times

--- frontend/src/LegInfo.js
+++ frontend/src/LegInfo.js
@@ -1,8 +1,47 @@
+import { useEffect, useState } from 'react';
+
 function LegInfo(props) {
+
+    const [departureTime, setDepartureTime] = useState(null);
+    const [arrivalTime, setArrivalTime] = useState(null);
+
+    
+    useEffect(() => {
+        let temp = parseInt(props.leg.departureTime.replace(/:/g, ''));
+        if(temp > 1259) {
+            temp = temp - 1200;
+            if(temp < 1000) {
+                setDepartureTime(temp.toString().slice(0,1) + ":" + temp.toString().slice(1,4) + "PM")
+            } else {
+                setDepartureTime(temp.toString().slice(0,2) + ":" + temp.toString().slice(2,4) + "PM")
+            }
+        } else {
+                setDepartureTime(temp.toString().slice(0,2) + ":" + temp.toString().slice(2,4) + "AM")
+        }
+        temp = parseInt(props.leg.arrivalTime.replace(/:/g, ''));
+        if(temp > 1259) {
+            temp = temp - 1200
+            if(temp > 1159) {
+                setArrivalTime(temp.toString().slice(0,2) + ":" + temp.toString().slice(2,4) + "PM")
+            } else if(temp < 1000) {
+                setArrivalTime(temp.toString().slice(0,1) + ":" + temp.toString().slice(1,4) + "PM")
+            } else {
+                if(temp > 1159) {
+                    setArrivalTime(temp.toString().slice(0,2) + ":" + temp.toString().slice(2,4) + "PM")
+                } else {
+                setArrivalTime(temp.toString().slice(0,2) + ":" + temp.toString().slice(2,4) + "PM")
+            } } }
+        else {
+            setArrivalTime(temp.toString().slice(0,2) + ":" + temp.toString().slice(2,4) + "AM")
+        }
+        temp = 0;
+    }
+    )
+
     return (
         <div className='flight-info-box'>
             <div>
-                <div>{props.leg.departureTime}-{props.leg.arrivalTime}</div>
+                <div>{departureTime}-{arrivalTime}</div>
                 <div>{props.leg.duration}</div>
             </div>
             <div>
