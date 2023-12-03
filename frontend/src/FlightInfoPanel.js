5bbf34b3dbb2158fa6139e2019c1e2bbdf317f9b
ChrisShobe <chrisshobe2000@gmail.com>
2023-12-02 16:34:42 -0800

    left side info with use states

--- frontend/src/FlightInfoPanel.js
+++ frontend/src/FlightInfoPanel.js
@@ -1,12 +1,18 @@
-
+import {useState } from 'react';
 
 function FlightInfoPanel() {
+    const [flightInfo, setFlightInfo] = useState("");
+    const [flightNumber, setFlightNumber] = useState("");
+    const [depDate, setDepDate] = useState("");
+    const [depTime, setDepTime] = useState("");
+    const [arrDate, setArrDate] = useState("");
+    const [arrTime, setArrTime] = useState("");
     return (
         <div className="FlightInfoPanel">
-            <h2>Flight Info</h2>
-            <h2>Flight Number</h2>
-            <h2>Departure Date and Time</h2>
-            <h2>Arrival Date and Time</h2>
+            <h2>Flight Info: {flightInfo}</h2>
+            <h2>Flight Number: {flightNumber}</h2>
+            <h2>Departure Date: {depDate}, Time: {depTime}</h2>
+            <h2>Arrival Date: {arrDate}, Time: {arrTime}</h2>
         </div>
     )
 }
