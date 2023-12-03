6be595cfecd50057fbacdf8331953d6d2aaa90cf
sipneat <jake.esperson@outlook.com>
2023-12-02 16:49:50 -0800

    create SelectTransport component and its items

--- frontend/src/SelectTransport.js
+++ frontend/src/SelectTransport.js
@@ -1,9 +1,17 @@
-
+import { useState } from 'react';
+import SelectTransportItem from './SelectTransportItem';
 
 function SelectTransport() {
+    const [mode, setMode] = useState('Driving');
+    const [leaveBy, setLeaveBy] = useState('9:50');
+    const [duration, setDuration] = useState('32');
+    const [cost, setCost] = useState('200');
+
     return (
         <div className="SelectTransport">
             <h2>Select Transport</h2>
+            <SelectTransportItem Mode={ mode } LeaveBy={ leaveBy } Duration={ duration } Cost={ cost } />
+            <SelectTransportItem Mode="Driving" LeaveBy="9:50" Duration="32" Cost="200" />
         </div>
     );
 }
